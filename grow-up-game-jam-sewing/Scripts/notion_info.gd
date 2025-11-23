00af89f0a6d9d1e61e84ee8efae1370db64f3e54
PigletP <emily.r.taylor2002@gmail.com>
2025-11-23 11:54:02 -0800

    Edits to notion

--- grow-up-game-jam-sewing/Scripts/notion_info.gd
+++ grow-up-game-jam-sewing/Scripts/notion_info.gd
@@ -17,7 +17,6 @@ func _ready() -> void:
 		5:
 			type = "ThreadAndNeedle"
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
