4d0f21ed5eebec12aa36a8af5a4e6b675b94ef0e
Andree Mejia Gonzalez <andremejia64@gmail.com>
2025-11-22 19:40:05 -0800

    signal connect error

--- grow-up-game-jam-sewing/MultiplayerManager.gd
+++ grow-up-game-jam-sewing/MultiplayerManager.gd
@@ -8,14 +8,14 @@ var playerIDs = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var multiplayer_api = get_tree().get_multiplayer()
-	multiplayer_api.peer_connected.connect("on_peer_connected")
-	multiplayer_api.peer_disconnected.connect("on_peer_disconnected")
-	if not multiplayer_api.is_server():
-		multiplayer_api.connected_to_server.connect("on_connected_to_server")
-		multiplayer_api.connection_failed.connect("on_connection_failed")
-		multiplayer_api.server_disconnected.connect("on_server_disconnected")
-	else:
-		playerIDs.append(multiplayer_api.get_unique_id())
+	multiplayer_api.peer_connected.connect(on_peer_connected)
+	#multiplayer_api.peer_disconnected.connect("on_peer_disconnected")
+	#if not multiplayer_api.is_server():
+		##multiplayer_api.connected_to_server.connect("on_connected_to_server")
+		##multiplayer_api.connection_failed.connect("on_connection_failed")
+		##multiplayer_api.server_disconnected.connect("on_server_disconnected")
+	#else:
+		#playerIDs.append(multiplayer_api.get_unique_id())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -45,4 +45,5 @@ func _on_tube_client_session_joined() -> void:
 	
  
 func on_peer_connected(id: int):
+	print("Player added: ", id);
 	playerIDs.append(id)
