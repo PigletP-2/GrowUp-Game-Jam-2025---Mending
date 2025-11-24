1a47f2825304cb649fa4c6e4fb4bf7339b752050
Andree Mejia Gonzalez <andremejia64@gmail.com>
2025-11-23 19:06:49 -0800

    gave up

--- grow-up-game-jam-sewing/Multiplayer.gd
+++ grow-up-game-jam-sewing/Multiplayer.gd
@@ -2,14 +2,19 @@ extends Node2D
 
 const PORT = 123
 const SERVER_ADDRESS = "localhost"
+const CARD_BODY_REF = preload("res://card_body.tscn")
+const CLOTH_REF = preload("res://clothing_info.tscn")
+const NOTION_REF = preload("res://notion_info.tscn")
 
+var ticket_manager
 var peer =  ENetMultiplayerPeer.new()
 var peerIDs = []
 
 #@export var player_field_scene : PackedScene
 #@export var player_2_field_scene : PackedScene
 
-
+func _ready() -> void:
+	ticket_manager = $Node2D/TicketManager
 
 func _on_host_button_pressed() -> void:
 	disable_button()
@@ -58,6 +63,24 @@ func print_own_id():
 	if !multiplayer.is_server(): 
 		print("Received RPC at: ", multiplayer.get_unique_id())
 	
+@rpc("any_peer","unreliable","call_remote")
+func _spawn_thrown_card(velocity: Variant, type: Variant, notion_type: Variant, notions: Variant, position: Variant):
+	print("try spawn")
+	var item = CARD_BODY_REF.instantiate()
+	ticket_manager.add_child(item)
+	item.position = position
+	item.apply_central_impulse(velocity)
+	var info
+	if type == "clothing":
+		info = CLOTH_REF.instantiate()
+		item.add_child(info)
+		info.notions = notions
+	else:
+		info = NOTION_REF.instantiate()
+		item.add_child(info)
+		info.notion_type = notion_type
+		
+		
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
@@ -68,9 +91,13 @@ func _input(event):
 			print(peerIDs)
 
 
-func _on_card_body_exit_left() -> void:
-	print("it got emitted")
+func _on_card_body_exit_left(signaler: Variant, velocity: Variant, type: Variant, notion_type: Variant, notions: Variant, position: Variant) -> void:
+	signaler.queue_free()
+	print("receive exit left")
+	_spawn_thrown_card.rpc_id(peerIDs[0], velocity, type, notion_type, notions, Vector2(648,position.y))
 
 
-func _on_card_body_exit_right() -> void:
-	pass # Replace with function body.
+func _on_card_body_exit_right(signaler: Variant, velocity: Variant, type: Variant, notion_type: Variant, notions: Variant, position: Variant) -> void:
+	print("receive exit right")
+	signaler.queue_free()
+	_spawn_thrown_card.rpc_id(peerIDs[0], velocity, type, notion_type, notions, Vector2(0,position.y))
--- grow-up-game-jam-sewing/Scripts/card_body.gd
+++ grow-up-game-jam-sewing/Scripts/card_body.gd
@@ -1,18 +1,19 @@
 extends RigidBody2D
 
 signal clicked
-signal exit_left
-signal exit_right
+signal exit_left(signaler, velocity, type, notion_type, notions, position)
+signal exit_right(signaler, velocity, type, notion_type, notions, position)
 
 var held = false
 var friction = Vector2(10,10)
 var type
 var child
 
+var scene_root
+
 func _ready():
 	contact_monitor = true
 	max_contacts_reported = 10
-	
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -43,9 +44,16 @@ func _process(delta: float) -> void:
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	print("emit oob signal")
 	if global_position.x < 0:
 		print("exit left")
-		exit_left.emit()
+		if type == "clothing":
+			exit_left.emit(self,linear_velocity,type,"blank",child.notions, global_position)
+		else:
+			exit_left.emit(self,linear_velocity,type,child.notion_type,null, global_position)
 	elif global_position.x > 648:
-		exit_right.emit()
+		if type == "clothing":
+			exit_right.emit(self,linear_velocity,type,"blank",child.notions, global_position)
+		else:
+			exit_right.emit(self,linear_velocity,type,child.notion_type,null, global_position)
 		print("exit right")
